dcc9e8d2d172ed4f44b8f49b7923aba76d04670b
Aaron Gao <gaoaaron1@gmail.com>
2024-06-26 08:22:35 -0600

    fixed backend

--- backend/app.py
+++ backend/app.py
@@ -12,7 +12,7 @@ similarity = pickle.load(open('artifacts/similarity.pkl', 'rb'))
 
 # Function to fetch movie poster path from TMDB API
 def fetch_poster(movie_id):
-    url = f"https://api.themoviedb.org/3/movie/{movie_id}?api_key=8265bd1679663a7ea12ac168da84d2e8&language=en-US"
+    url = f"https://api.themoviedb.org/3/movie/{movie_id}?api_key=YOUR_TMDB_API_KEY&language=en-US"  # Replace with your TMDB API key
     data = requests.get(url).json()
     poster_path = data['poster_path']
     return f"https://image.tmdb.org/t/p/w500/{poster_path}"
@@ -20,4 +20,32 @@ def fetch_poster(movie_id):
 # Route to get list of movie titles 
 @app.route('/movies', methods=['GET'])
 def get_movies():
-    movie_titles
+    movie_titles = movies['title'].tolist()   # Get list of movie titles from loaded data
+    return jsonify(movie_titles)              # Return JSON response containing movie titles
+
+# Route to recommend movies based on similarity to given movie
+@app.route('/recommend', methods=['GET'])
+def recommend():
+    movie_title = request.args.get('title')   # Get 'title' parameter from query string
+    index = movies[movies['title'] == movie_title].index[0]   # Find index of given movie title in data
+    
+    # Sort movies by similarity to the given movie
+    distances = sorted(list(enumerate(similarity[index])), reverse=True, key=lambda x: x[1])
+    
+    recommended_movies = []   # Initialize empty list to store recommended movies
+    
+    # Retrieve details and posters for top 5 similar movies
+    for i in distances[1:6]:
+        movie_id = movies.iloc[i[0]]['movie_id']     # Get movie ID of recommended movie
+        poster = fetch_poster(movie_id)              # Fetch poster URL using TMDB API
+        recommended_movies.append({
+            "name": movies.iloc[i[0]]['title'],      # Add movie title to recommended list
+            "poster": poster                         # Add poster URL to recommended list
+        })
+    
+    return jsonify(recommended_movies)    # Return JSON response with recommended movies and posters
+
+#================================== MAIN =======================================================
+
+if __name__ == '__main__':
+    app.run(debug=True)   # Run Flask application in debug mode if executed directly
