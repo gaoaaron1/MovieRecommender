8c6923f05be9cbb3fb7bd4fdb101f2313a5af870
Aaron Gao <gaoaaron1@gmail.com>
2024-06-26 08:11:03 -0600

    fixed front end

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,11 @@
-// vite.config.js
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    outDir: 'dist'  // Ensures Vite outputs to the 'dist' directory
+  },
   define: {
     'process.env': process.env
   }
